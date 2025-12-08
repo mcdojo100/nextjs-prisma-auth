307886c95538c7b8d339385f86094f2e0f90b7e9
mcdojo100 <mcdojo100@yahoo.com>
2025-12-08 03:53:38 -0800

    File upload updates

--- src/app/api/uploads/route.ts
+++ src/app/api/uploads/route.ts
@@ -1,6 +1,10 @@
 import { NextResponse } from 'next/server'
 import { put } from '@vercel/blob'
 
+export const config = {
+  runtime: 'nodejs', // instead of Edge
+}
+
 export async function POST(request: Request) {
   const formData = await request.formData()
   const files = formData.getAll('files') as File[]
@@ -12,12 +16,15 @@ export async function POST(request: Request) {
   try {
     const urls: string[] = []
     for (const file of files) {
-      const { url } = await put(`uploads/${file.name}`, file, {
+      const arrayBuffer = await file.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+      const { url } = await put(`uploads/${file.name}`, buffer, {
         access: 'public',
         addRandomSuffix: true,
       })
       urls.push(url)
     }
+
     return NextResponse.json({ urls })
   } catch (err) {
     console.error('Upload failed', err)
